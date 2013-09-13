282b6cd77c72896432ea83713d9ad9ed8252fd0d
veleary <veleary@gmail.com>
2013-09-13 10:28:08 -0500

    Fixed methods and initialization tests

--- tennis.rb
+++ tennis.rb
@@ -9,6 +9,14 @@ module Tennis
       @player1.opponent = @player2
       @player2.opponent = @player1
     end
+
+    def wins_ball(player_num)
+      if player_num == 1
+        player1.points+=1
+      else 
+        player2.points+=1
+      end
+    end
   end
 
   class Player
@@ -22,12 +30,41 @@ module Tennis
     #
     # Returns the integer new score.
     def record_won_ball!
-      @points += 1
+        @points += 1
     end
 
     # Returns the String score for the player.
     def score
-      return 'love' if @points == 0
+      if @points == 0 
+        return 'love'       
+      elsif @points == 1
+        return 'fifteen'      
+      elsif @points == 2 
+        return 'thirty'      
+      else 
+        return 'forty'          
+      end
     end
   end
-end
+end 
+
+
+# def scoring
+        
+#       elsif (player1.points >= 3 && player1.opponent.points >= 3) && (player1 - player1.opponent == 1){
+#         return 'advantage for #{player}'
+#       }
+#       elsif (player1.points >= 3 && player1.opponent.points >= 3) && (player1.points == player1.opponent.points) {
+#         return 'deuce'
+#       }
+#       else {
+#         return 'Play Again!'
+#       }
+#       end    
+#     end
+
+      
+
+
+
+
